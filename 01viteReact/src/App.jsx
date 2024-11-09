358f45eebba1c5c97f3e61835003cc20cf5f8ac2
Satyam <www.satyam200413@gmail.com>
2024-11-09 11:24:09 +0530

    Adding all my files of first lecture

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Chai Aur React || Satyam Tripathi</h1>
+    <div>
+      <p>
+        Inside this lecture I got to learn about how to create react project and the difference between react and vite+react
+      </p>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
