5ff1188decd17100ff9d0236b7672a71eae293cf
Satyam <www.satyam200413@gmail.com>
2024-11-11 08:44:53 +0530

    Completed my third lecture on creating my own custom react

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -4,9 +4,10 @@ import Chai from "./chai";
 function App() {
   // const [count, setCount] = useState(0)
 
+  const userName = "Satyam Tripathi"
   return (
     <>
-    <h1>Chai Aur React || Satyam Tripathi</h1>
+    <h1>Chai Aur React || {userName}</h1>
     <div>
       <p>
         Inside this lecture I got to learn about how to create react project and the difference between react and vite+react
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
+// import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App | chai</h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement = {
+//   type:'a',
+//   props:{
+//       href:'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+// const reactElement =createElement(
+//   'a',
+//   {href:'https://google.com',target: '_blank'},
+//   'click me to visit google'
+// )
+
+
+// const anotherElement = (
+//   <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <App/>
+  
 )
