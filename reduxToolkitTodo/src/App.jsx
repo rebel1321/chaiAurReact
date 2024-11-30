4b5ed4c3b1db21ff0fdaf78b0054190cb0679194
Satyam <www.satyam200413@gmail.com>
2024-11-30 14:31:44 +0530

    Completed my project using redux toolkit,where we used createSlice,dispatcher,selector,reducer

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      
+      <h2 className='text-center text-2xl font-semibold'>Learn about redux toolkit</h2>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
