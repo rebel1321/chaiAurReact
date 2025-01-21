af81579698e857944a2ae90564f144b5dd8cbdad
Satyam <www.satyam200413@gmail.com>
2025-01-21 13:19:09 +0530

    completed the cors part and manage the bug

--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,9 +1,11 @@
 const conf={
 
-appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
-appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
-appwriteDatabaseId:String(import.meta.env.VITE_DATABASE_ID),
-appwriteCollectionId:String(import.meta.env.VITE_COLLECTION_ID),
-appwriteBucketId:String(import.meta.env.VITE_BUCKET_ID)
-}
+appwriteUrl:"https://cloud.appwrite.io/v1",
+appwriteProjectId:"678f35ce001d7dcfa127",
+appwriteDatabaseId:"678f36a50011b5449a21",
+appwriteCollectionId:"678f36ed0034db5dc886",
+appwriteBucketId:"678f38400015a1471db0"
+};
+console.log(conf.appwriteProjectId)
+
 export default conf
