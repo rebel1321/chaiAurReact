96525cacf204f097dfc30cff37cb847acc059091
Satyam <www.satyam200413@gmail.com>
2025-01-23 12:55:46 +0530

    Debug the Home.jsx file where we are getting the error of fetching post.length

--- 12MegaBlog/src/pages/Home.jsx
+++ 12MegaBlog/src/pages/Home.jsx
@@ -1,48 +1,72 @@
-
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
+import { Container, PostCard } from '../components';
 import { Link } from 'react-router-dom';
 
 function Home() {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-  
-    if (posts.length === 0) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <Link to="/login"><h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
-                            </h1></Link>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        )
-    }
+  useEffect(() => {
+    appwriteService.getPosts()
+      .then((response) => {
+        if (response && response.documents) {
+          setPosts(response.documents);
+        } else {
+          setPosts([]); // Ensure posts is always an array
+        }
+      })
+      .catch((err) => {
+        console.error('Error fetching posts:', err);
+        setError('Failed to fetch posts. Please try again later.');
+      });
+  }, []);
+
+  if (error) {
     return (
-        <div className='w-full py-8'>
-            <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
-            </Container>
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold text-red-500">{error}</h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  if (!posts || posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <Link to="/login">
+                <h1 className="text-2xl font-bold hover:text-gray-500">
+                  Login to read posts
+                </h1>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
